76087c2d44b149b0b7d25fc7ba53bafad427215e
Artur Nechurenko <49311617+artur-nech@users.noreply.github.com>
2020-08-06 11:47:36 -0400

    5 homework

---
+++ JS_HW/2_JS_HW5.js
@@ -0,0 +1,8 @@
+/*2. Удалите первый и последний элемент из массива:
+arr = [ "a", "b", "c", "d", "e" ]
+
+ */
+var arr = [ "a", "b", "c", "d", "e" ];
+arr.splice(0, 1);
+arr.splice (length.arr-1, 1);
+console.log (arr);

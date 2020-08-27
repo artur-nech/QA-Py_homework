e4ef16506856c426695ac79fef8443f02d926764
Artur Nechurenko <49311617+artur-nech@users.noreply.github.com>
2020-08-27 12:24:43 -0400

    first2089

--- HW_Python/Py HW1/N1.py
+++
@@ -1,3 +0,0 @@
-a = 11
-b = 23
-print("b is greater than a") if b >= a else print("b is not greater than a")
--- HW_Python/Py HW1/N2.py
+++
@@ -1,6 +0,0 @@
-a = 1
-b = '0'
-c = '3'
-d = bool(b)
-e = bool(c)
-print (a, b, c, d, e)
